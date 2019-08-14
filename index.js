b2f720a8c32cbc8e41df533412a25283cd128572
jackbolton901 <bltnjck@gmail.com>
2019-08-14 16:20:41 -0500

    Switch work 08/14/2019

---
+++ index.js
@@ -0,0 +1,38 @@
+let userPut= prompt("Enter 1,2,3,4,or 5")
+switch(userPut)
+{
+    case "1":
+        alert("1");
+        break;
+    case "2":
+        alert("2");
+        break;
+    case"3":
+        alert("3");
+        break;
+    case "4":
+        alert("4");
+        break;
+    case "5":
+        alert("5")
+}
+let binary=prompt("enter 3 digit binary number")
+switch(binary)
+{
+    case "1":
+        alert("1");
+        break;
+    case "10":
+        alert("2");
+        break;
+    case"11":
+        alert("3");
+        break;
+    case"100":
+        alert("4");
+        break;
+    case"101":
+        alert("5")
+}
+
+
